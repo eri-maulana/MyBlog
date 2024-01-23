a7f03c59e14b8f9f2649c93aee1cb61380771de3
eri-maulana <maulanabouble11@gmail.com>
2024-01-23 10:17:51 +0700

    2. Blade Templating Engine

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   <title>My Blog | About</title>
-</head>
-<body>
+@extends('layouts.main')
+
+@section('container')
    <h1>Ini Halaman About</h1>
    <h2>{{ $nama; }}</h2>
    <h2>{{ $email; }}</h2>
    <h2>{{ $jurusan; }}</h2>
-</body>
-</html>
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,12 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   <title>My Blog | Posts</title>
-</head>
-<body>
-   <h1>Ini Halaman Posts</h1>
-</body>
-</html>
+@extends('layouts.main')
+
+@section('container')
+   <h1>Ini Halaman Post</h1>
+
+   @foreach ($posts as $post)
+   <h2>
+      <a href="/posts/{{ $post['slug'] }}">{{ $post['title'] }}</a>
+   </h2>
+   <h5>{{ $post['author'] }}</h5>
+   <p>{{ $post['body'] }}</p>
+   @endforeach
+@endsection
