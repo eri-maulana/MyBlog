52ec3b2432b266ba0c91f8fd4b7e6a0229880a10
eri-maulana <maulanabouble11@gmail.com>
2024-01-24 18:05:32 +0700

    6. Post Category & Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,9 @@
 @section('container')
 
    <h2>{{ $post->title }}</h2>
-   {!! $post->body !!}
 
-<a href="/posts">Back to All Posts</a>
+   <p>By. Eri Maulana in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
+   {!! $post->body !!}
+   <a href="/posts">Back to All Posts</a>
 @endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -14,8 +14,15 @@
 {{-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi fugit tenetur officia saepe impedit iste laborum nihil quaerat voluptatum itaque pariatur ad tempore est omnis veritatis inventore repellat, explicabo eum quas!</p><p> Enim itaque cum ab eos, voluptatem neque laborum doloremque rerum obcaecati quod veritatis ratione autem labore, reiciendis odio alias fugit velit expedita facilis! Quia quibusdam libero consectetur quasi nostrum asperiores architecto quaerat aut, dignissimos quas, id sequi sunt officiis culpa nemo sit eius sint inventore!</p><p> Eum aperiam veniam natus voluptatibus error. Voluptatum a assumenda numquam perspiciatis sed reiciendis minus, totam beatae, voluptas dolorum consectetur, iure veniam aliquid facilis dolores animi esse. Labore omnis error architecto quasi laborum! Itaque magni necessitatibus fugit inventore recusandae accusantium ipsum labore libero optio excepturi?</p>
 
 Post::create([
-   'title' => 'Post Ketiga',
-   'slug' => 'post-ketiga',
+   'title' => 'Post Lima',
+   'category_id' => 3,
+   'slug' => 'post-lima',
    'excerpt' => 'Lorem ipsum dolor sit amet',
    'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi fugit tenetur officia saepe impedit iste laborum nihil quaerat voluptatum itaque pariatur ad tempore est omnis veritatis inventore repellat, explicabo eum quas!</p><p> Enim itaque cum ab eos, voluptatem neque laborum doloremque rerum obcaecati quod veritatis ratione autem labore, reiciendis odio alias fugit velit expedita facilis! Quia quibusdam libero consectetur quasi nostrum asperiores architecto quaerat aut, dignissimos quas, id sequi sunt officiis culpa nemo sit eius sint inventore!</p><p> Eum aperiam veniam natus voluptatibus error. Voluptatum a assumenda numquam perspiciatis sed reiciendis minus, totam beatae, voluptas dolorum consectetur, iure veniam aliquid facilis dolores animi esse. Labore omnis error architecto quasi laborum! Itaque magni necessitatibus fugit inventore recusandae accusantium ipsum labore libero optio excepturi?</p>'
-]) --}}
+]) 
+
+Category::create([
+   'name' => 'Personal',
+   'slug' => 'personal'
+])
+--}}
