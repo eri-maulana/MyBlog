2b2d5544884e7361223991cac55460277523ee41
eri-maulana <maulanabouble11@gmail.com>
2024-01-25 06:59:27 +0700

    7. Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,10 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Category;
+use App\Models\User;
+use App\Models\Post;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,9 +18,64 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Pembuatan User / Author
+        User::create([
+            'name' => 'Eri Maulana',
+            'email' => 'eri@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+        User::create([
+            'name' => 'Putri Narila',
+            'email' => 'putri@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        // Pembuatan Category
+        Category::create([
+            'name' => 'Programming',
+            'slug' => 'programming'
+        ]);
+        Category::create([
+            'name' => 'Web Design',
+            'slug' => 'web-design'
+        ]);
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        // Pembuatan data posts
+        Post::create([
+            'title' => 'Post Ke Satu',
+            'category_id' => 1,
+            'slug' => 'post-ke-satu',
+            'excerpt' => 'Lorem ipsum dolor sit amet',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi fugit tenetur officia saepe impedit iste laborum nihil quaerat voluptatum itaque pariatur ad tempore est omnis veritatis inventore repellat, explicabo eum quas!</p><p> Enim itaque cum ab eos, voluptatem neque laborum doloremque rerum obcaecati quod veritatis ratione autem labore, reiciendis odio alias fugit velit expedita facilis! Quia quibusdam libero consectetur quasi nostrum asperiores architecto quaerat aut, dignissimos quas, id sequi sunt officiis culpa nemo sit eius sint inventore!</p><p> Eum aperiam veniam natus voluptatibus error. Voluptatum a assumenda numquam perspiciatis sed reiciendis minus, totam beatae, voluptas dolorum consectetur, iure veniam aliquid facilis dolores animi esse. Labore omnis error architecto quasi laborum! Itaque magni necessitatibus fugit inventore recusandae accusantium ipsum labore libero optio excepturi?</p>',
+            'user_id' => 1
+        ]);
+        Post::create([
+            'title' => 'Post Ke Dua',
+            'category_id' => 1,
+            'slug' => 'post-ke-dua',
+            'excerpt' => 'Lorem ipsum dolor sit amet',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi fugit tenetur officia saepe impedit iste laborum nihil quaerat voluptatum itaque pariatur ad tempore est omnis veritatis inventore repellat, explicabo eum quas!</p><p> Enim itaque cum ab eos, voluptatem neque laborum doloremque rerum obcaecati quod veritatis ratione autem labore, reiciendis odio alias fugit velit expedita facilis! Quia quibusdam libero consectetur quasi nostrum asperiores architecto quaerat aut, dignissimos quas, id sequi sunt officiis culpa nemo sit eius sint inventore!</p><p> Eum aperiam veniam natus voluptatibus error. Voluptatum a assumenda numquam perspiciatis sed reiciendis minus, totam beatae, voluptas dolorum consectetur, iure veniam aliquid facilis dolores animi esse. Labore omnis error architecto quasi laborum! Itaque magni necessitatibus fugit inventore recusandae accusantium ipsum labore libero optio excepturi?</p>',
+            'user_id' => 1
+        ]);
+        Post::create([
+            'title' => 'Post Ke Tiga',
+            'category_id' => 2,
+            'slug' => 'post-ke-tiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi fugit tenetur officia saepe impedit iste laborum nihil quaerat voluptatum itaque pariatur ad tempore est omnis veritatis inventore repellat, explicabo eum quas!</p><p> Enim itaque cum ab eos, voluptatem neque laborum doloremque rerum obcaecati quod veritatis ratione autem labore, reiciendis odio alias fugit velit expedita facilis! Quia quibusdam libero consectetur quasi nostrum asperiores architecto quaerat aut, dignissimos quas, id sequi sunt officiis culpa nemo sit eius sint inventore!</p><p> Eum aperiam veniam natus voluptatibus error. Voluptatum a assumenda numquam perspiciatis sed reiciendis minus, totam beatae, voluptas dolorum consectetur, iure veniam aliquid facilis dolores animi esse. Labore omnis error architecto quasi laborum! Itaque magni necessitatibus fugit inventore recusandae accusantium ipsum labore libero optio excepturi?</p>',
+            'user_id' => 2
+        ]);
+        Post::create([
+            'title' => 'Post Ke Empat',
+            'category_id' => 3,
+            'slug' => 'post-ke-empat',
+            'excerpt' => 'Lorem ipsum dolor sit amet',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi fugit tenetur officia saepe impedit iste laborum nihil quaerat voluptatum itaque pariatur ad tempore est omnis veritatis inventore repellat, explicabo eum quas!</p><p> Enim itaque cum ab eos, voluptatem neque laborum doloremque rerum obcaecati quod veritatis ratione autem labore, reiciendis odio alias fugit velit expedita facilis! Quia quibusdam libero consectetur quasi nostrum asperiores architecto quaerat aut, dignissimos quas, id sequi sunt officiis culpa nemo sit eius sint inventore!</p><p> Eum aperiam veniam natus voluptatibus error. Voluptatum a assumenda numquam perspiciatis sed reiciendis minus, totam beatae, voluptas dolorum consectetur, iure veniam aliquid facilis dolores animi esse. Labore omnis error architecto quasi laborum! Itaque magni necessitatibus fugit inventore recusandae accusantium ipsum labore libero optio excepturi?</p>',
+            'user_id' => 2
+        ]);
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,28 +1,20 @@
 @extends('layouts.main')
 
 @section('container')
-   <h1>Ini Halaman Post</h1>
+    @foreach ($posts as $post)
+        <article class="py-2">
+            <h2>
+                <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
+            </h2>
 
-   @foreach ($posts as $post)
-   <h2>
-      <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
-   </h2>
-   <p>{{ $post->excerpt }}</p>
-   @endforeach
+            <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a
+                    href="/categories/{{ $post->category->slug }}"
+                    class="text-decoration-none">{{ $post->category->name }}</a></p>
+
+            <p>{{ $post->excerpt }}</p>
+
+            <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read More ...</a>
+        </article>
+        <hr>
+    @endforeach
 @endsection
-
-{{-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi fugit tenetur officia saepe impedit iste laborum nihil quaerat voluptatum itaque pariatur ad tempore est omnis veritatis inventore repellat, explicabo eum quas!</p><p> Enim itaque cum ab eos, voluptatem neque laborum doloremque rerum obcaecati quod veritatis ratione autem labore, reiciendis odio alias fugit velit expedita facilis! Quia quibusdam libero consectetur quasi nostrum asperiores architecto quaerat aut, dignissimos quas, id sequi sunt officiis culpa nemo sit eius sint inventore!</p><p> Eum aperiam veniam natus voluptatibus error. Voluptatum a assumenda numquam perspiciatis sed reiciendis minus, totam beatae, voluptas dolorum consectetur, iure veniam aliquid facilis dolores animi esse. Labore omnis error architecto quasi laborum! Itaque magni necessitatibus fugit inventore recusandae accusantium ipsum labore libero optio excepturi?</p>
-
-Post::create([
-   'title' => 'Post Lima',
-   'category_id' => 3,
-   'slug' => 'post-lima',
-   'excerpt' => 'Lorem ipsum dolor sit amet',
-   'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi fugit tenetur officia saepe impedit iste laborum nihil quaerat voluptatum itaque pariatur ad tempore est omnis veritatis inventore repellat, explicabo eum quas!</p><p> Enim itaque cum ab eos, voluptatem neque laborum doloremque rerum obcaecati quod veritatis ratione autem labore, reiciendis odio alias fugit velit expedita facilis! Quia quibusdam libero consectetur quasi nostrum asperiores architecto quaerat aut, dignissimos quas, id sequi sunt officiis culpa nemo sit eius sint inventore!</p><p> Eum aperiam veniam natus voluptatibus error. Voluptatum a assumenda numquam perspiciatis sed reiciendis minus, totam beatae, voluptas dolorum consectetur, iure veniam aliquid facilis dolores animi esse. Labore omnis error architecto quasi laborum! Itaque magni necessitatibus fugit inventore recusandae accusantium ipsum labore libero optio excepturi?</p>'
-]) 
-
-Category::create([
-   'name' => 'Personal',
-   'slug' => 'personal'
-])
---}}
