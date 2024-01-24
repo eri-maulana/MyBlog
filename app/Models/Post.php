0eaab30b65384d8f5929b8f91bbd5d4875cd0d0e
eri-maulana <maulanabouble11@gmail.com>
2024-01-24 15:55:12 +0700

    5. Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,39 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Post Pertama",
-            "slug" => "post-pertama",
-            "author" => "Eri Maulana",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, at. Repudiandae blanditiis maxime in saepe veritatis. Ducimus tenetur atque corporis officia, quaerat ex eius aliquid animi totam voluptas ea sapiente porro, labore fugit sit laudantium. Eos, maxime reiciendis voluptate at pariatur accusantium illum aut, vitae officiis est, quisquam deleniti quae odit soluta fugit voluptates possimus nemo minus corporis rem omnis! Soluta exercitationem natus in molestiae enim expedita dignissimos fugiat voluptas, iste aspernatur quibusdam dolorum! Tempora ut fugit incidunt voluptatibus facilis!"
-        ],
-        [
-            "title" => "Post Kedua",
-            "slug" => "post-kedua",
-            "author" => "Putri Narila",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab blanditiis commodi pariatur, tempore temporibus perferendis accusamus unde ullam quidem quod consequuntur praesentium explicabo veritatis sequi, suscipit repudiandae voluptatibus! Illum sapiente corrupti nostrum facilis architecto ullam recusandae nihil in. Reprehenderit ipsam voluptates aut labore consequatur laudantium magnam est explicabo debitis culpa quisquam, beatae omnis a iure voluptatibus obcaecati officiis aliquam id quia necessitatibus repellendus minima odio fuga! Quia aliquid beatae asperiores est et consequuntur alias quasi illum sint nihil dicta ducimus nesciunt nisi quam fuga odio illo velit sapiente quod provident, animi voluptate. Quos dolor quibusdam maxime error, tempora nemo incidunt."
-        ]
-    ];
+    use HasFactory;
 
-    public static function all()
-    {
-        return collect(self::$blog_posts);
-    }
-
-
-    public static function find($slug)
-    {
-        $posts = static::all();
-        $post = [];
-        foreach ($posts as $p) {
-            if ($p['slug'] == $slug) {
-                $post = $p;
-            }
-        }
-        return $post;
-    }
+    protected $guarded = ['id'];
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,9 +5,17 @@
 
    @foreach ($posts as $post)
    <h2>
-      <a href="/posts/{{ $post['slug'] }}">{{ $post['title'] }}</a>
+      <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
    </h2>
-   <h5>{{ $post['author'] }}</h5>
-   <p>{{ $post['body'] }}</p>
+   <p>{{ $post->excerpt }}</p>
    @endforeach
 @endsection
+
+{{-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi fugit tenetur officia saepe impedit iste laborum nihil quaerat voluptatum itaque pariatur ad tempore est omnis veritatis inventore repellat, explicabo eum quas!</p><p> Enim itaque cum ab eos, voluptatem neque laborum doloremque rerum obcaecati quod veritatis ratione autem labore, reiciendis odio alias fugit velit expedita facilis! Quia quibusdam libero consectetur quasi nostrum asperiores architecto quaerat aut, dignissimos quas, id sequi sunt officiis culpa nemo sit eius sint inventore!</p><p> Eum aperiam veniam natus voluptatibus error. Voluptatum a assumenda numquam perspiciatis sed reiciendis minus, totam beatae, voluptas dolorum consectetur, iure veniam aliquid facilis dolores animi esse. Labore omnis error architecto quasi laborum! Itaque magni necessitatibus fugit inventore recusandae accusantium ipsum labore libero optio excepturi?</p>
+
+Post::create([
+   'title' => 'Post Ketiga',
+   'slug' => 'post-ketiga',
+   'excerpt' => 'Lorem ipsum dolor sit amet',
+   'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi fugit tenetur officia saepe impedit iste laborum nihil quaerat voluptatum itaque pariatur ad tempore est omnis veritatis inventore repellat, explicabo eum quas!</p><p> Enim itaque cum ab eos, voluptatem neque laborum doloremque rerum obcaecati quod veritatis ratione autem labore, reiciendis odio alias fugit velit expedita facilis! Quia quibusdam libero consectetur quasi nostrum asperiores architecto quaerat aut, dignissimos quas, id sequi sunt officiis culpa nemo sit eius sint inventore!</p><p> Eum aperiam veniam natus voluptatibus error. Voluptatum a assumenda numquam perspiciatis sed reiciendis minus, totam beatae, voluptas dolorum consectetur, iure veniam aliquid facilis dolores animi esse. Labore omnis error architecto quasi laborum! Itaque magni necessitatibus fugit inventore recusandae accusantium ipsum labore libero optio excepturi?</p>'
+]) --}}
