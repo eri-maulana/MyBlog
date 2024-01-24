631d46e1f21b67aa82c3e6efe4230adec751fc47
eri-maulana <maulanabouble11@gmail.com>
2024-01-24 07:59:33 +0700

    4. Database, Migration & Eloquent

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            // eri = 12345
+            // putri = 54321
             $table->rememberToken();
             $table->timestamps();
         });
